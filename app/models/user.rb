835d2d285ceffaa0dd0a317b3f4d5f263d0b4665
easilyamused07 <ramirezcarla21@yahoo.com>
2020-06-06 16:27:38 -0500

    added friendships model and friendship helper methods

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,9 @@
 class User < ApplicationRecord
   before_save {self.email = email.downcase}
   has_many :messages
+  has_many :friendships
+  has_many :inverse_friendships, :class_name => "Friendship",
+	  :foreign_key => "friend_id"
   validates :username, presence: true, uniqueness: {case_sensitive: false},
 	  length: {minimum: 3, maximum: 25}
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
@@ -8,4 +11,31 @@ class User < ApplicationRecord
                     uniqueness: { case_sensitive: false },
                     format: { with: VALID_EMAIL_REGEX }
   has_secure_password
+
+    def friends
+    friends_array = friendships.map{|friendship| friendship.friend if friendship.confirmed}
+    friends_array + inverse_friendships.map{|friendship| friendship.user if friendship.confirmed}
+    friends_array.compact
+  end
+
+  # Users who have yet to confirme friend requests
+  def pending_friends
+    friendships.map{|friendship| friendship.friend if !friendship.confirmed}.compact
+  end
+
+  # Users who have requested to be friends
+  def friend_requests
+    inverse_friendships.map{|friendship| friendship.user if !friendship.confirmed}.compact
+  end
+
+  def confirm_friend(user)
+    friendship = inverse_friendships.find{|friendship| friendship.user == user}
+    friendship.confirmed = true
+    friendship.save
+  end
+
+  def friend?(user)
+    friends.include?(user)
+  end
+
 end
