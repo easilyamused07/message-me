a93a2894050c8ca7eae19e0f551f9dc2410cdaf9
easilyamused07 <ramirezcarla21@yahoo.com>
2020-06-10 21:45:44 -0500

    inverse friendships added to model

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,6 +3,10 @@ class User < ApplicationRecord
   has_many :messages
   has_many :friendships
   has_many :friends, :through => :friendships
+
+  has_many :inverse_friendships, :class_name => "Friendship", :foreign_key => "friend_id"
+  has_many :inverse_friends, :through => :inverse_friendships, :source => :user
+
   validates :username, presence: true, uniqueness: {case_sensitive: false},
 	  length: {minimum: 3, maximum: 25}
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
